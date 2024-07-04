af2b2b5643e64dc589ba5024f1e7cc1746570350
Shirui Chen <chen.shir@northeastern.edu>
2024-07-04 16:31:44 -0700

    modified input component for lab1

--- Components/Input.js
+++ Components/Input.js
@@ -1,19 +1,35 @@
 import { View, Text, TextInput } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Input = () => {
   const [text, setText] = useState("");
+  const [thankyouVisible, setThankyouVisible] = useState(false);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleBlur = () => {
+    setThankyouVisible(true);
+  };
+
+  const handleChangeText = (changedText) => {
+    setText(changedText);
+    setThankyouVisible(false);
+  };
 
   return (
     <View>
       <TextInput
         value={text}
         placeholder="Type something"
-        autoCapitalize={true}
-        onChangeText={function (changedText) {
-          setText(changedText);
-        }}
+        autoCapitalize={"none"}
+        onChangeText={handleChangeText}
+        onBlur={handleBlur}
+        ref={inputRef}
       />
+      {thankyouVisible && <Text>Thank you</Text>}
     </View>
   );
 };
