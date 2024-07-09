7d8866cb3f7229303f5b121201872288d74e6ebf
Shirui Chen <chen.shir@northeastern.edu>
2024-07-09 14:30:39 -0700

    Wrapped input in a Modal. Added a button in App to show the input as Modal. Hide the Modal when user presses Confirm.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import Header from "./Components/Header";
 import Input from "./Components/Input";
 import React, { useState } from "react";
@@ -7,21 +7,24 @@ import React, { useState } from "react";
 export default function App() {
   const appName = "Summer 2024 class";
   const [receivedText, setReceivedText] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
 
   function handleInputData(data) {
     console.log("callback fn called with", data);
     setReceivedText(data);
+    setModalVisible(false);
   }
 
   return (
     <View style={styles.container}>
       <Header name={appName} theme="dark">
-        <Text>Child 1</Text>
-        <Text>Child 2</Text>
+        {/* <Text>Child 1</Text> */}
+        {/* <Text>Child 2</Text> */}
       </Header>
-      <Input inputHandler={handleInputData} />
+      <Input inputHandler={handleInputData} isModalVisible={modalVisible} />
       <Text>{receivedText}</Text>
       <StatusBar style="auto" />
+      <Button title="Add a goal" onPress={() => setModalVisible(true)} />
     </View>
   );
 }
--- Components/Input.js
+++ Components/Input.js
@@ -1,15 +1,17 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Modal, StyleSheet } from "react-native";
 import React, { useState, useEffect, useRef } from "react";
 import { Button } from "react-native";
 
-const Input = ({ inputHandler }) => {
+const Input = ({ inputHandler, isModalVisible }) => {
   const [text, setText] = useState("");
   const [thankyouVisible, setThankyouVisible] = useState(false);
-  const inputRef = useRef();
+  const inputRef = useRef(null);
 
   useEffect(() => {
-    inputRef.current.focus();
-  }, []);
+    if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, [isModalVisible]);
 
   const handleBlur = () => {
     setThankyouVisible(true);
@@ -26,24 +28,35 @@ const Input = ({ inputHandler }) => {
   }
 
   return (
-    <View>
-      <TextInput
-        value={text}
-        placeholder="Type something"
-        autoCapitalize={"none"}
-        onChangeText={handleChangeText}
-        onBlur={handleBlur}
-        ref={inputRef}
-      />
-      {thankyouVisible && <Text>Thank you</Text>}
-      <Button
-        title="Confirm"
-        onPress={() => {
-          handleConfirm();
-        }}
-      />
-    </View>
+    <Modal animationType="slide" visible={isModalVisible}>
+      <View style={styles.container}>
+        <TextInput
+          value={text}
+          placeholder="Type something"
+          autoCapitalize={"none"}
+          onChangeText={handleChangeText}
+          onBlur={handleBlur}
+          ref={inputRef}
+        />
+        {thankyouVisible && <Text>Thank you</Text>}
+        <Button
+          title="Confirm"
+          onPress={() => {
+            handleConfirm();
+          }}
+        />
+      </View>
+    </Modal>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default Input;
