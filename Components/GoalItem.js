885aba0ace2253ddbb432aad303880a90a3e0b71
Shirui Chen <chen.shir@northeastern.edu>
2024-07-18 13:37:05 -0700

    Wrapped GoalItem in a pressable

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Pressable } from 'react-native';
 
 const GoalItem = ({ goal, deleteHandler }) => {
   const navigation = useNavigation();
 
   return (
     <View style={styles.textContainer}>
+        <Pressable style={styles.pressable} onPress={() => navigation.navigate('Details', { goalObj: goal })}>
       <Text style={styles.textStyle}>{goal.text}</Text>
       <View style={styles.buttonStyle}>
         <Button
@@ -14,12 +16,12 @@ const GoalItem = ({ goal, deleteHandler }) => {
           title="X"
           onPress={() => deleteHandler(goal.id)}
         />
-        <Button
+        {/* <Button
           color="black"
           title="i"
-          onPress={() => navigation.navigate('Details', { goalObj: goal })}
-        />
+        /> */}
       </View>
+      </Pressable>
     </View>
   );
 };
@@ -29,14 +31,17 @@ const styles = StyleSheet.create({
     color: 'darkmagenta',
     fontSize: 25,
   },
+  pressable:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+  },
   textContainer: {
     color: 'darkmagenta',
     backgroundColor: '#aaa',
     marginVertical: 15,
     borderRadius: 5,
-    padding: 15,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
   },
   buttonStyle: {
     flexDirection: 'row',
