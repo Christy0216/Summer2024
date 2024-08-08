70ee0dd5fda861173ff0e594cdcb02b8002aea31
Shirui Chen <chen.shir@northeastern.edu>
2024-08-08 14:00:32 -0700

    receive onpress data added a marker based on that and added a button to pass the data back to profile

--- Components/Map.js
+++ Components/Map.js
@@ -1,25 +1,42 @@
-import { View, Text } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import React from "react";
-import MapView from "react-native-maps";
-import { StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { useState } from "react";
 
-export default function Map() {
+export default function Map({navigation}) {
+  const [selectedLocation, setSelectedLocation] = useState(null);
   return (
-    <MapView
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-      style={styles.map}
-    />
+    <>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={styles.map}
+        onPress={(e) => {
+          setSelectedLocation({
+            latitude: e.nativeEvent.coordinate.latitude,
+            longitude: e.nativeEvent.coordinate.longitude,
+          });
+        }}
+      >
+        <Marker title="Picked Location" coordinate={selectedLocation} />
+      </MapView>
+      <Button
+        title="Confirm Selected Location"
+        onPress={() => {
+          navigation.navigate("Profile", { selectedLocation });
+        }}
+        disabled={!selectedLocation}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   map: {
-    width: "100%",
-    height: "100%",
+    flex: 1,
   },
 });
